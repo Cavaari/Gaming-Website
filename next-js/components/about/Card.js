626b812f5f49a130d6f31efb60b3ecfa610336cf
Egor Ivanov <eivanov@uoguelph.ca>
2024-01-30 17:05:21 -0500

    about me cards

--- next-js/components/about/Card.js
+++ next-js/components/about/Card.js
@@ -1,21 +1,23 @@
 import Image from "next/image"
 import Link from "next/link"
 
-export default function Card({name, imgSrc, description}) {
+export default function Card({ name, imgSrc, description }) {
 
     return (
-        <div class="card p-4 col-12 col-md-2">
-            <Image
+        <div class="col">
+            <div className="card m-1">
+                <Image
                     src={imgSrc}
                     alt="Picture of the author"
                     class="card-img-top"
-                    fill="true"
+                    width={150}
+                    height={250}
                 />
-            <div class="card-body">
-                <h5 class="card-title">{name}</h5>
-                <p class="card-text">{description}</p>
-                <Link href="" class="btn btn-primary">Go somewhere</Link>
+                <div class="card-body">
+                    <h5 class="card-title">{name}</h5>
+                    <p class="card-text">{description}</p>
+                    <Link href="" class="btn btn-primary">Go somewhere</Link>
+                </div>
             </div>
-            {name}
         </div>)
 }
