257cde8322ddaa93f7385205258d487c13f80056
Egor Ivanov <eivanov@uoguelph.ca>
2024-01-30 19:58:06 -0500

    Cavaari pages done

--- next-js/pages/about.js
+++ next-js/pages/about.js
@@ -6,8 +6,8 @@ export default function About() {
             <h1 style={{fontSize: "5rem"}} className="text-first text-center align-self-md-start">About Us</h1>
             <div class="mt-3 mb-3 w-100 row row-cols-1 row-cols-md-3 row-cols-lg-6 g-2 d-flex  justify-content-around">
                 <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Eric's description" link="/about/edearing" />
-                <Card name="Egor" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" descirption="Egor's description" link="/about/eivanov"/>
-                <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" />
+                <Card name="Egor" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Egor's description" link="/about/eivanov"/>
+                <Card name="Cavaari" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Cavaari's description" link="/about/cavaari" />
                 <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" />
                 <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" />
                 <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" />
--- next-js/pages/about/cavaari.js
+++ next-js/pages/about/cavaari.js
@@ -1,9 +1,32 @@
+import Head from "next/head"
 import Image from "next/image"
+import { useEffect } from "react"
 
 export default function About() {
+    useEffect(()=>{
+        document.getElementById('currentYear').innerText = new Date().getFullYear();
+    },[])
     return (
         <>
-            
+            <Head>
+                <link rel="stylesheet" type="text/css" href="/about/cavaari.css"/>
+            </Head>
+            <main>
+                <div id="about" class="form-section">
+                    <h2>About Me</h2>
+                    <p>Hi, I'm Cavaari Taylor. I love web development and programming.
+                        I hail from the island of Saint Lucia in the Caribbean. A lovely island
+                        in the Caribbean where the sun is always shining and the people are always smiling.
+                        I am currently in my final semester of my Software Engineering degree at the University of Guelph.
+                        I am a very hardworking and dedicated individual who is always willing to learn new things.
+                        I am in my 4th year at the University of Guelph and I am eager to break into the industry.
+                    </p>
+                </div>
+
+            </main>
+            <footer>
+                <p>&copy; <span id="currentYear"></span> Cavaari</p>
+            </footer>
         </>
     )
 }
