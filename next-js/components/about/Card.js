ac3bdf9005e79f31ad9feecc1fef3c778e272fba
Egor Ivanov <eivanov@uoguelph.ca>
2024-01-31 11:15:05 -0500

    about us cards no image improvment

--- next-js/components/about/Card.js
+++ next-js/components/about/Card.js
@@ -1,18 +1,33 @@
 import Image from "next/image"
 import Link from "next/link"
 
+function randomColor(){
+    const colors = ["#E9A400", "#152A64", "#00032F"]
+    const random = Math.floor(Math.random() * colors.length);
+    return colors[random]
+}
+
 export default function Card({ name, link = "", imgSrc, description }) {
 
     return (
         <div className="col">
             <div className="card m-1">
-                <Image
-                    src={imgSrc}
-                    alt="Picture of the author"
-                    className="card-img-top"
-                    width={150}
-                    height={200}
-                />
+                {
+                    imgSrc ? (
+                        <Image
+                            src={imgSrc}
+                            alt="Picture of the author"
+                            className="card-img-top"
+                            width={150}
+                            height={200}
+                        />
+                    ) : (
+                        <div className="rounded-top d-flex align-items-center justify-content-center" style={{width:"100%", height:"200px", backgroundColor: randomColor(), fontSize:"4rem", color: "white"}}>
+                            <span style={{textShadow: "2px 2px #000000"}}>{name[0]}</span>
+                        </div>
+                    )
+                }
+
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">{description}</p>
--- next-js/pages/about.js
+++ next-js/pages/about.js
@@ -5,14 +5,14 @@ export default function About() {
         <div className="p-5 bg-third d-flex flex-column align-items-center mx-auto">
             <h1 style={{fontSize: "5rem"}} className="text-first text-center align-self-md-start">About Us</h1>
             <div className="mt-3 mb-3 w-100 row row-cols-1 row-cols-md-3 row-cols-lg-4 g-2 d-flex  justify-content-around">
-                <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Eric's description" link="/about/edearing" />
-                <Card name="Egor" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Egor's description" link="/about/eivanov"/>
-                <Card name="Cavaari" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Cavaari's description" link="/about/cavaari" />
-                <Card name="Karina" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Karina's description" link="/about/kgarmend" />
-                <Card name="Kalindu" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Kalindu's description" link="/about/kkehelba" />
-                <Card name="Muhammad" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Muhammad's description" link="/about/msalmaan" />
-                <Card name="Tehreem" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Tehreem's description" link="/about/tnazar"  />
-                <Card name="Greg" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" />
+                <Card name="Eric" imgSrc="" description="Eric's description" link="/about/edearing" />
+                <Card name="Egor" imgSrc="" description="Egor's description" link="/about/eivanov"/>
+                <Card name="Cavaari" imgSrc="" description="Cavaari's description" link="/about/cavaari" />
+                <Card name="Karina" imgSrc="" description="Karina's description" link="/about/kgarmend" />
+                <Card name="Kalindu" imgSrc="" description="Kalindu's description" link="/about/kkehelba" />
+                <Card name="Muhammad" imgSrc="" description="Muhammad's description" link="/about/msalmaan" />
+                <Card name="Tehreem" imgSrc="" description="Tehreem's description" link="/about/tnazar"  />
+                <Card name="Greg" imgSrc="" />
             </div>
         </div>
     )
