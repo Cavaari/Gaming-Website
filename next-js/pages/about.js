e429cad18a71dfef8bb41856ba018f3931fb9fbe
Egor Ivanov <eivanov@uoguelph.ca>
2024-01-30 20:05:19 -0500

    Karina page done

--- next-js/pages/about.js
+++ next-js/pages/about.js
@@ -2,15 +2,17 @@ import Card from "@/components/about/Card";
 
 export default function About() {
     return (
-        <div className="p-3 bg-third d-flex flex-column align-items-center mx-auto">
+        <div className="p-5 bg-third d-flex flex-column align-items-center mx-auto">
             <h1 style={{fontSize: "5rem"}} className="text-first text-center align-self-md-start">About Us</h1>
-            <div class="mt-3 mb-3 w-100 row row-cols-1 row-cols-md-3 row-cols-lg-6 g-2 d-flex  justify-content-around">
+            <div class="mt-3 mb-3 w-100 row row-cols-1 row-cols-md-3 row-cols-lg-4 g-2 d-flex  justify-content-around">
                 <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Eric's description" link="/about/edearing" />
                 <Card name="Egor" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Egor's description" link="/about/eivanov"/>
                 <Card name="Cavaari" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Cavaari's description" link="/about/cavaari" />
+                <Card name="Karina" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" description="Karina's description" link="/about/kgarmend" />
                 <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" />
                 <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" />
                 <Card name="Eric" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" />
+                <Card name="Greg" imgSrc="/runeTextures/Blue/Rectangle/runeBlue_rectangle_001.png" />
             </div>
         </div>
     )
--- next-js/pages/about/kgarmend.js
+++ next-js/pages/about/kgarmend.js
@@ -1,9 +1,19 @@
-import Image from "next/image"
-
+import Head from "next/head"
 export default function About() {
     return (
         <>
-            
+             <Head>
+                <title>Karina Garmendez</title>
+                <link rel="stylesheet" type="text/css" href="/about/kgarmend.css"/>
+            </Head>
+            <section>
+                <div class="sec" id="sec">
+                    <h2>About Me</h2>
+                    <p>I'm in my final academic semester of 5th year Software Engineering Co-op here at UofG. I began coding in my 1st year at Guelph, and love using computers
+                        to not only solve complex problems, but to create and add my skills to new things. I finished my minor in music this past F23 semester and specialized in jazz guitar.
+                    </p>
+                </div>
+            </section>
         </>
     )
 }
