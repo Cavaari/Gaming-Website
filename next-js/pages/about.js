57ff719deddeffcc1891367a07aa0e4053b60418
Eric Dearing <edearing@uoguelph.ca>
2024-02-04 22:45:25 -0500

    added about me card

--- next-js/pages/about.js
+++ next-js/pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
         <div className="p-5 bg-third d-flex flex-column align-items-center mx-auto">
             <h1 style={{fontSize: "5rem"}} className="text-first text-center align-self-md-start">About Us</h1>
             <div className="mt-3 mb-3 w-100 row row-cols-1 row-cols-md-3 row-cols-lg-4 g-2 d-flex  justify-content-around">
-                <Card name="Eric" imgSrc="" description="Eric's description" link="/about/edearing" />
+                <Card name="Eric" imgSrc="/about/eric.jpg" description="IT Manager & Software Developer at Perpetual Motion Sports and creator of 3ric Games" link="/about/edearing" />
                 <Card name="Egor" imgSrc="" description="Egor's description" link="/about/eivanov"/>
                 <Card name="Cavaari Taylor" imgSrc="/about/Profile Photo.jpg" description="An island boy with a passion for programming!" link="/about/cavaari" />
                 <Card name="Karina" imgSrc="/about/karina.png" description="I mostly enjoy backend development and HCI!" link="/about/kgarmend" />
