1fd43818929091f1eaad446dd52bc80db19f0ca4
Kalindu Kehel Baddarage <59674165+kalindukehel@users.noreply.github.com>
2024-02-05 11:15:50 -0500

    Add about me

--- next-js/pages/about.js
+++ next-js/pages/about.js
@@ -9,7 +9,7 @@ export default function About() {
                 <Card name="Egor" imgSrc="" description="Egor's description" link="/about/eivanov"/>
                 <Card name="Cavaari Taylor" imgSrc="/about/Memoji.jpeg" description="An island boy with a passion for programming!" link="/about/cavaari" />
                 <Card name="Karina" imgSrc="/about/karina.png" description="I mostly enjoy backend development and HCI!" link="/about/kgarmend" />
-                <Card name="Kalindu" imgSrc="" description="Kalindu's description" link="/about/kkehelba" />
+                <Card name="Kalindu" imgSrc="/about/kalindu.png" description="I like playing basketball and making apps!" link="/about/kkehelba" />
                 <Card name="Muhammad" imgSrc="/about/Muhammad.png" description="Muhammad's description" link="/about/msalmaan" />
                 <Card name="Tehreem" imgSrc="" description="Tehreem's description" link="/about/tnazar"  />
                 <Card name="Greg" imgSrc="" /> 
