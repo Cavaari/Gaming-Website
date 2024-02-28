6414db9263210eb115d5ca2022bc2c13ad140d57
Egor Ivanov <eivanov@uoguelph.ca>
2024-02-28 14:38:28 -0500

    code splitted in proper components' files

--- next-js/pages/wordle.js
+++ next-js/pages/wordle.js
@@ -1,83 +1,10 @@
 import SocketContext from '@/components/SocketContext';
-import WinnerModal from '@/components/wordle/WinnerModal';
-import LoserModal from '@/components/wordle/LoserModal';
 import { useContext, useEffect, useRef, useState } from 'react';
 
-const square = {
-    width: 50,
-    height: 50,
-    margin: 5,
-    border: "1px solid #dedede",
-    backgroundColor: "#ebebeb",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    boxShadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;",
-    fontWeight: "bold",
-    fontSize: 24,
-    // webkitTextStrokeWidth: 1,
-    // webkitTextStrokeColor: "#000000"
-}
+import Board from '@/components/wordle/Board';
 
-function Square({ value }) {
-    return (
-        <div style={square}>
-            {value}
-        </div>
-    );
-}
-
-function Board({ trials }) {
-
-    return (
-        <div className="d-flex flex-column align-items-center">
-            <h1 className='mb-5'>Welcome to Wordle Game</h1>
-            <div id="row-1" className="d-flex">
-                <Square value={trials[0][0]} />
-                <Square value={trials[0][1]} />
-                <Square value={trials[0][2]} />
-                <Square value={trials[0][3]} />
-                <Square value={trials[0][4]} />
-            </div>
-            <div id="row-2" className="d-flex">
-                <Square value={trials[1][0]} />
-                <Square value={trials[1][1]} />
-                <Square value={trials[1][2]} />
-                <Square value={trials[1][3]} />
-                <Square value={trials[1][4]} />
-            </div>
-            <div id="row-3" className="d-flex">
-                <Square value={trials[2][0]} />
-                <Square value={trials[2][1]} />
-                <Square value={trials[2][2]} />
-                <Square value={trials[2][3]} />
-                <Square value={trials[2][4]} />
-            </div>
-            <div id="row-4" className="d-flex">
-                <Square value={trials[3][0]} />
-                <Square value={trials[3][1]} />
-                <Square value={trials[3][2]} />
-                <Square value={trials[3][3]} />
-                <Square value={trials[3][4]} />
-            </div>
-            <div id="row-5" className="d-flex">
-                <Square value={trials[4][0]} />
-                <Square value={trials[4][1]} />
-                <Square value={trials[4][2]} />
-                <Square value={trials[4][3]} />
-                <Square value={trials[4][4]} />
-            </div>
-            <div id="row-6" className="d-flex">
-                <Square value={trials[5][0]} />
-                <Square value={trials[5][1]} />
-                <Square value={trials[5][2]} />
-                <Square value={trials[5][3]} />
-                <Square value={trials[5][4]} />
-            </div>
-        </div>
-    );
-}
-
+import WinnerModal from '@/components/wordle/WinnerModal';
+import LoserModal from '@/components/wordle/LoserModal';
 
 export default function Wordle() {
     const winnerModalRef = useRef(null)
