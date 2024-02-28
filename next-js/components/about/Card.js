938758d622ad6e043592ae7ecd4ec0e69ba4adca
Karina Garmendez <kgarmend@uoguelph.ca>
2024-02-28 14:31:51 -0500

    Added contact email in card footer

--- next-js/components/about/Card.js
+++ next-js/components/about/Card.js
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from 'next/router'
 
 function randomColor(){
     const colors = ["#E9A400", "#152A64", "#00032F"]
@@ -7,8 +8,8 @@ function randomColor(){
     return colors[random]
 }
 
-export default function Card({ name, link = "", imgSrc, description }) {
-
+export default function Card({ name, link = "", imgSrc, description, contact }) {
+    const router = useRouter()
     return (
         <div className="col">
             <div className="card m-1">
@@ -34,6 +35,9 @@ export default function Card({ name, link = "", imgSrc, description }) {
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">{description}</p>
                     <Link href={link} className="btn btn-primary">{name}&apos;s Page</Link>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Contact at {contact}</small>
                 </div>
             </div>
         </div>)
