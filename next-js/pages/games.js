3d7f12181d70d74afa3d01269a9e12e0b26175bd
Eric Dearing <edearing@uoguelph.ca>
2024-03-01 18:03:58 -0500

    added pictures to main page

--- next-js/pages/games.js
+++ next-js/pages/games.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Image from 'next/image';
+import matchPic from '../public/mainPage/match.png';
+import wordlePic from '../public/mainPage/wordle.png';
 
 const Games = () => {
   // Simulated navigation function (replace with actual navigation logic in a real app)
@@ -15,6 +18,10 @@ const Games = () => {
             <div className="card-body">
               <h2 className="card-title">Card Matching</h2>
               <p className="card-text">Test your memory with this matching game.</p>
+              <div className="h-50 d-flex flex-column align-items-center justify-content-center">
+                <Image src={matchPic} width={350} height={300} />
+              </div>
+
             </div>
           </div>
         </div>
@@ -23,6 +30,7 @@ const Games = () => {
             <div className="card-body">
               <h2 className="card-title">Wordle</h2>
               <p className="card-text">Challenge your vocabulary with Wordle.</p>
+              <Image src={wordlePic} width={350} height={391} />
             </div>
           </div>
         </div>
