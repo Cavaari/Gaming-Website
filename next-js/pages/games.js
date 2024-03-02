0b43c317c75103556d1c77d6ec63c44ee78fdfe8
Cavaari <cavaarit@gmail.com>
2024-03-01 22:55:24 -0500

    additional games.js styling

--- next-js/pages/games.js
+++ next-js/pages/games.js
@@ -7,15 +7,14 @@ const Games = () => {
   const [isHoveredMatch, setIsHoveredMatch] = useState(false);
   const [isHoveredWordle, setIsHoveredWordle] = useState(false);
 
-  
   const navigateToGame = (url) => {
-    window.location.href = url; 
+    window.location.href = url;
   };
 
-  // Hover Scale Style
+  // Style object for transforming scale on hover
   const hoverStyle = {
     transform: 'scale(1.05)',
-    transition: 'transform 0.3s ease-in-out', 
+    transition: 'transform 0.3s ease-in-out', // Only transforming scale, assuming your CSS handles background color transition
   };
 
   return (
@@ -24,13 +23,13 @@ const Games = () => {
       <div className="row justify-content-center">
         <div className="col-md-4">
           <div 
-            className="card mb-4 shadow-sm h-100" 
+            className={`card mb-4 shadow-sm h-100 card-hover-transition ${isHoveredMatch ? 'bg-first' : 'bg-second'}`} 
             onClick={() => navigateToGame('/game')} 
             style={{ cursor: 'pointer', ...(isHoveredMatch ? hoverStyle : {}) }}
             onMouseEnter={() => setIsHoveredMatch(true)}
             onMouseLeave={() => setIsHoveredMatch(false)}
           >
-            <div className="card-body">
+            <div className="card-body text-white">
               <h2 className="card-title">Card Matching</h2>
               <p className="card-text">Test your memory with this matching game.</p>
               <div className="h-50 d-flex flex-column align-items-center justify-content-center">
@@ -41,13 +40,13 @@ const Games = () => {
         </div>
         <div className="col-md-4">
           <div 
-            className="card mb-4 shadow-sm h-100" 
+            className={`card mb-4 shadow-sm h-100 card-hover-transition ${isHoveredWordle ? 'bg-first' : 'bg-second'}`} 
             onClick={() => navigateToGame('/wordle')} 
             style={{ cursor: 'pointer', ...(isHoveredWordle ? hoverStyle : {}) }}
             onMouseEnter={() => setIsHoveredWordle(true)}
             onMouseLeave={() => setIsHoveredWordle(false)}
           >
-            <div className="card-body">
+            <div className="card-body text-white">
               <h2 className="card-title">Wordle</h2>
               <p className="card-text">Challenge your vocabulary with Wordle.</p>
               <Image src={wordlePic} width={350} height={391} />
