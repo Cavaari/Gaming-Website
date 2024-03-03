a9fed57ad1df9f00bcade62c5560df70bd98403e
Egor Ivanov <eivanov@uoguelph.ca>
2024-03-03 11:54:56 -0500

    games cards visuals are improved

--- next-js/pages/games.js
+++ next-js/pages/games.js
@@ -18,38 +18,43 @@ const Games = () => {
   };
 
   return (
-    <div className="container-fluid text-center bg-third new-section text-first" style={{ minHeight: '100vh' }}>
-      <h1 className="mb-4">Select a Game</h1>
+    <div className="h-100 bg-third text-first text-center container-fluid">
+      <h1 className="p-3 mb-4">Select a Game</h1>
       <div className="row justify-content-center">
-        <div className="col-md-4">
-          <div 
-            className={`card mb-4 shadow-sm h-100 card-hover-transition ${isHoveredMatch ? 'bg-first' : 'bg-second'}`} 
-            onClick={() => navigateToGame('/game')} 
+        <div className="col-11 col-md-4" style={{ marginBottom: "3.9rem" }}>
+          <div
+            className={`card mb-4 shadow-sm h-100 card-hover-transition ${isHoveredMatch ? 'bg-first' : 'bg-second'}`}
+            onClick={() => navigateToGame('/game')}
             style={{ cursor: 'pointer', ...(isHoveredMatch ? hoverStyle : {}) }}
             onMouseEnter={() => setIsHoveredMatch(true)}
             onMouseLeave={() => setIsHoveredMatch(false)}
           >
-            <div className="card-body text-white">
+            <div className="card-body text-white d-flex flex-column">
               <h2 className="card-title">Card Matching</h2>
               <p className="card-text">Test your memory with this matching game.</p>
-              <div className="h-50 d-flex flex-column align-items-center justify-content-center">
-                <Image src={matchPic} width={350} height={300} />
+
+              <div className='align-self-center h-100 d-flex align-items-center justify-content-center'>
+                <Image className='img-fluid' src={matchPic} width={350} height={300} />
               </div>
+
+
             </div>
           </div>
         </div>
-        <div className="col-md-4">
-          <div 
-            className={`card mb-4 shadow-sm h-100 card-hover-transition ${isHoveredWordle ? 'bg-first' : 'bg-second'}`} 
-            onClick={() => navigateToGame('/wordle')} 
+        <div className="col-11 col-md-4" style={{ marginBottom: "3.9rem" }}>
+          <div
+            className={`card mb-4 shadow-sm h-100 card-hover-transition ${isHoveredWordle ? 'bg-first' : 'bg-second'}`}
+            onClick={() => navigateToGame('/wordle')}
             style={{ cursor: 'pointer', ...(isHoveredWordle ? hoverStyle : {}) }}
             onMouseEnter={() => setIsHoveredWordle(true)}
             onMouseLeave={() => setIsHoveredWordle(false)}
           >
-            <div className="card-body text-white">
+            <div className="card-body text-white d-flex flex-column">
               <h2 className="card-title">Wordle</h2>
               <p className="card-text">Challenge your vocabulary with Wordle.</p>
-              <Image src={wordlePic} width={350} height={391} />
+              <div className='align-self-center h-100 d-flex align-items-center justify-content-center'>
+                <Image className='img-fluid' src={wordlePic} width={350} height={391} />
+              </div>
             </div>
           </div>
         </div>
