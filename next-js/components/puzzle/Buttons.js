632f7c0cc7969f573ee10ae3aeb6275a6503d7b5
Cavaari <cavaarit@gmail.com>
2024-03-10 18:42:22 -0400

    Styling changes

--- next-js/components/puzzle/Buttons.js
+++ next-js/components/puzzle/Buttons.js
@@ -45,26 +45,68 @@ export default function Buttons({ setWinnerIndex, gameState, handleUserInputSubm
     }
 
     return (
-        <div id='buttons_game' style={mainStyle}>
-            {gameState &&
-                <span className='text-center mb-3'>{JSON.stringify(gameState.buttons.hint)}</span>
+        <div id='buttons_game' className="buttons-game">
+          {gameState &&
+            <div className="hint-text">{gameState.buttons.hint}</div>
+          }
+          <div className="btn-group" role="group" aria-label="Button group with nested radio toggle button group">
+            <input disabled={colorsStr.includes("red")} onClick={() => handleColorClick("red")} type="radio" className="btn-check" name="vbtn-radio" id="vbtn-radio1" autoComplete="off" />
+            <label className="btn btn-danger custom-btn" htmlFor="vbtn-radio1">Red</label>
+            
+            <input disabled={colorsStr.includes("yellow")} onClick={() => handleColorClick("yellow")} type="radio" className="btn-check" name="vbtn-radio" id="vbtn-radio2" autoComplete="off" />
+            <label className="btn btn-warning custom-btn" htmlFor="vbtn-radio2">Yellow</label>
+            
+            <input disabled={colorsStr.includes("green")} onClick={() => handleColorClick("green")} type="radio" className="btn-check" name="vbtn-radio" id="vbtn-radio3" autoComplete="off" />
+            <label className="btn btn-success custom-btn" htmlFor="vbtn-radio3">Green</label>
+            
+            <input disabled={colorsStr.includes("white")} onClick={() => handleColorClick("white")} type="radio" className="btn-check" name="vbtn-radio" id="vbtn-radio4" autoComplete="off" />
+            <label className="btn btn-light custom-btn" htmlFor="vbtn-radio4">White</label>
+          </div>
+          <div className="answer-section">
+            <h5>Your Answer</h5>
+            <div className="answer-display">{colorsStr}</div>
+            <button onClick={handleSubmit} className='btn btn-primary submit-btn'>Submit Answer</button>
+          </div>
+      
+          <style jsx>{`
+            .buttons-game {
+              text-align: center;
+              padding: 20px;
             }
-            <div className="btn-group" style={buttonStyle.default} role="group" aria-label="Vertical radio toggle button group">
-                <input disabled={colorsStr.includes("red")} onClick={() => handleColorClick("red")} type="radio" className="btn-check" name="vbtn-radio" id="vbtn-radio1" autoComplete="off"></input>
-                <label className="btn btn-danger" htmlFor="vbtn-radio1">Red</label>
-                <input disabled={colorsStr.includes("yellow")} onClick={() => handleColorClick("yellow")} type="radio" className="btn-check" name="vbtn-radio" id="vbtn-radio2" autoComplete="off"></input>
-                <label className="btn btn-warning" htmlFor="vbtn-radio2">Yellow</label>
-                <input disabled={colorsStr.includes("green")} onClick={() => handleColorClick("green")} type="radio" className="btn-check" name="vbtn-radio" id="vbtn-radio3" autoComplete="off"></input>
-                <label className="btn btn-success" htmlFor="vbtn-radio3">Green</label>
-                <input disabled={colorsStr.includes("white")} onClick={() => handleColorClick("white")} type="radio" className="btn-check" name="vbtn-radio" id="vbtn-radio4" autoComplete="off"></input>
-                <label className="btn btn-light" htmlFor="vbtn-radio4">White</label>
-            </div><br></br>
-            <div style={{ textAlign: 'center' }}>
-                <h5>Your Answer</h5>
-                {colorsStr}
-                <br></br>
-                <button onClick={handleSubmit} className='btn btn-primary m-5'>Submit Answer</button>
-            </div>
+      
+            .hint-text {
+              margin-bottom: 1rem;
+              font-size: 1.2rem;
+            }
+      
+            .btn-group {
+              display: flex;
+              justify-content: center;
+              margin-bottom: 1rem;
+            }
+      
+            .custom-btn {
+              margin: 0 0.5rem;
+              border: 2px solid black;
+              border-radius: 15px;
+            }
+      
+            .answer-section {
+              margin-top: 2rem;
+            }
+      
+            .answer-display {
+              font-weight: bold;
+              margin: 1rem 0;
+            }
+      
+            .submit-btn {
+              margin-top: 1rem;
+              border: 2px solid black;
+              border-radius: 15px; 
+            }
+          `}</style>
         </div>
-    )
+      );
+      
 }
