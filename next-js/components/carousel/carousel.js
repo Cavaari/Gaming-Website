cf2a654bb521c37c1654fa57352ef4cd1a310e6c
msalmaan <msalmaan@uoguelph.ca>
2024-03-14 23:31:03 -0400

    Carousel changes

--- next-js/components/carousel/carousel.js
+++ next-js/components/carousel/carousel.js
@@ -20,25 +20,24 @@ const GamesCarousel = () => {
       alt: 'Wordle Game',
       href: '/wordle',
     },
-    // Any other aditional games we will add here
+
   ];
 
   return (
     <Carousel>
       {games.map((game, index) => (
-        <Carousel.Item key={index}>
-          <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-            <Image
-              src={game.image}
-              alt={game.alt}
-              layout="fill"
-              objectFit="contain" // Maintain aspect ratio
-            />
-          </div>
-          <Carousel.Caption>
+        <Carousel.Item key={index} style={{position: 'relative', color: 'white', textAlign: 'center', height: '75vh'}}>
+          <Image src={game.image} alt={game.alt} layout="fill" objectFit="cover" />
+          <div style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 2
+          }}>
             <h3>{game.name}</h3>
             <p>{game.description}</p>
-          </Carousel.Caption>
+          </div>
         </Carousel.Item>
       ))}
     </Carousel>
