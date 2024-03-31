6b2802642b774c93c0a4935f272dd7c28f160ebc
Eric Dearing <edearing@uoguelph.ca>
2024-03-31 18:19:00 -0400

    added hovered variable for maze

--- next-js/pages/games.js
+++ next-js/pages/games.js
@@ -9,6 +9,7 @@ const Games = () => {
   const [isHoveredMatch, setIsHoveredMatch] = useState(false);
   const [isHoveredWordle, setIsHoveredWordle] = useState(false);
   const [isHoveredJeopardy, setIsHoveredJeopardy] = useState(false);
+  const [isHoveredMaze, setIsHoveredMaze] = useState(false);
 
   const navigateToGame = (url) => {
     window.location.href = url;
@@ -23,7 +24,7 @@ const Games = () => {
   return (
     <div className="bg-primary d-flex flex-column align-items-center justify-content-center">
       <div className="h-100 text-secondary text-center container-fluid">
-        <h1 className="p-3 mb-4 mt-5" style={{fontSize: "4rem"}} >Select a Game</h1>
+        <h1 className="p-3 mb-4 mt-5" style={{ fontSize: "4rem" }} >Select a Game</h1>
         <div className="row justify-content-center">
           <div className="col-11 col-md-4" style={{ marginBottom: "3.9rem" }}>
             <div
@@ -37,7 +38,7 @@ const Games = () => {
                 <h2 className="card-title">Card Matching</h2>
                 <p>Test your memory with card-matching. Can you beat our hardest level?</p>
                 <div className='align-self-center h-100 d-flex align-items-center justify-content-center'>
-                  <Image className='img-fluid rounded' src={matchPic} width={350} height={300} alt="GIF of Card Matching game"/>
+                  <Image className='img-fluid rounded' src={matchPic} width={350} height={300} alt="GIF of Card Matching game" />
                 </div>
               </div>
             </div>
@@ -54,7 +55,7 @@ const Games = () => {
                 <h2 className="card-title">Wordle</h2>
                 <p>Can you guess the word? Or is it too hard for you?</p>
                 <div className='align-self-center h-100 d-flex align-items-center justify-content-center'>
-                  <Image className='img-fluid rounded' src={wordlePic} width={350} height={391} alt="GIF of Wordle game"/>
+                  <Image className='img-fluid rounded' src={wordlePic} width={350} height={391} alt="GIF of Wordle game" />
                 </div>
               </div>
             </div>
@@ -71,32 +72,32 @@ const Games = () => {
                 <h2 className="card-title">Jeopardy</h2>
                 <p>Test your random fact knowledge. Play locally with up to 4 friends!</p>
                 <div className='align-self-center h-100 d-flex align-items-center justify-content-center'>
-                  <Image className='img-fluid rounded' src={jeopardyPic} width={350} height={391} alt="GIF with image of Jeopardy game"/>
+                  <Image className='img-fluid rounded' src={jeopardyPic} width={350} height={391} alt="GIF with image of Jeopardy game" />
                 </div>
               </div>
             </div>
           </div>
           <div className="col-11 col-md-4" style={{ marginBottom: "3.9rem" }}>
             <div
-              className={`card mb-4 shadow-sm h-100 card-hover-transition ${isHoveredJeopardy ? 'bg-first' : 'bg-second'}`}
-              // onClick={() => navigateToGame('/jeopardy')}
-              style={{ cursor: 'pointer', ...(isHoveredJeopardy ? hoverStyle : {}) }}
-              onMouseEnter={() => setIsHoveredJeopardy(true)}
-              onMouseLeave={() => setIsHoveredJeopardy(false)}
+              className={`card mb-4 shadow-sm h-100 card-hover-transition ${isHoveredMaze ? 'bg-first' : 'bg-second'}`}
+              onClick={() => navigateToGame('/maze')}
+              style={{ cursor: 'pointer', ...(isHoveredMaze ? hoverStyle : {}) }}
+              onMouseEnter={() => setIsHoveredMaze(true)}
+              onMouseLeave={() => setIsHoveredMaze(false)}
             >
               <div className="card-body text-secondary d-flex flex-column">
-                <h2 className="card-title">Coming Soon...</h2>
+                <h2 className="card-title">Maze</h2>
                 <p></p>
                 <div className='align-self-center h-100 d-flex align-items-center justify-content-center'>
-                  <Image className='img-fluid rounded' src={comingSoon} width={350} height={391} alt="GIF with text that says coming soon"/>
+                  <Image className='img-fluid rounded' src={comingSoon} width={350} height={391} alt="GIF with text that says coming soon" />
                 </div>
               </div>
             </div>
           </div>
         </div>
-        </div>
       </div>
-    
+    </div>
+
   );
 }
 
