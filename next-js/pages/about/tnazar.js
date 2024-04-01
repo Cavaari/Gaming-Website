3b36f38d562127aa19f26f21e9afc8d1b7f785f7
Tehreem404 <tehreemnazar1@gmail.com>
2024-04-01 03:50:45 -0400

    updated about me page

--- next-js/pages/about/tnazar.js
+++ next-js/pages/about/tnazar.js
@@ -1,34 +1,55 @@
-import Image from "next/image"
-import Head from "next/head"
-
+import Image from "next/image";
+import Head from "next/head";
 
 export default function About() {
     return (
         <>
             <Head>
                 <title>About Tehreem Nazar</title>
-                {/* eslint-disable */}
-                <link rel="stylesheet" type="text/css" href="/about/tnazar.css" />
-                {/* eslint-enable */}
             </Head>
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col-lg-4">
-                        <Image width={400} height="400" src="/about/Zeus.jpeg" alt="My Cat Zeus" className="img-fluid mb-3"/>
+            <div className="d-flex flex-column min-vh-100">
+                <div className="container my-5 flex-grow-1">
+                    <div style={{ backgroundColor: 'var(--about-bg-color)', color: 'var(--about-text-color)', borderRadius: '0.25rem', padding: '1rem', boxShadow: '0 0.3rem 0.25rem rgba(0, 0, 0, 0.075)' }} className="rounded shadow-sm">
+                        <div className="row align-items-center">
+                        <div className="col-lg-4 mb-4" style={{ backgroundColor: '#B47978', padding: '0px', borderRadius: '100%' }}> 
+                            <Image width={400} height={400} src="/about/tehreem.png" alt="My Cat Zeus" className="img-fluid rounded-circle border" style={{ borderColor: '#ddd', borderWidth: '5px' }}/>
+                        </div>
+                            <div className="col-lg-8">
+                                <h1 className="mb-3">Tehreem Nazar</h1>
+                                <h2 className="mb-3">About Me</h2>
+                                <p style={{ marginBottom: '20px' }}>I am a 5th year student studying Software Engineering at the University of Guelph. I enjoy backend development and currently work at National Defence as a Programmer Analyst. I have a cat that annoys me a lot and loves to sit on my keyboard when I'm programming. In my spare time, I enjoy reading philosophical books, trying out new cuisines and photography is something I've recently become interested in. Take a look at some of my amateur pictures below!</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-lg-8">
-                        <h1>Tehreem Nazar</h1>
-                        <h2>About Me</h2>
-                        <p>I am a 5th year student studying Software Engineering at the University of Guelph. I enjoy backend development and currently work at National Defence as a Programmer Analyst. I have a cat that annoys me a lot and loves to sit on my keyboard when I&apos;m programming (he&apos;s pictured above). In my spare time, I enjoy reading philosophical books and trying out new cuisines.</p>
+                </div>
+                <div className="container mt-2"> 
+                    <div className="row">
+                        <div className="col-md-4 mb-4">
+                            <img src="/about/photo1.JPG" alt="Photo 1" className="img-fluid rounded"/>
+                        </div>
+                        <div className="col-md-4 mb-4">
+                            <img src="/about/photo2.JPG" alt="Photo 1" className="img-fluid rounded"/>
+                        </div>
+                        <div className="col-md-4 mb-4">
+                            <img src="/about/photo3.JPG" alt="Photo 1" className="img-fluid rounded"/>
+                        </div>
+                        <div className="col-md-4 mb-4">
+                            <img src="/about/photo4.JPG" alt="Photo 1" className="img-fluid rounded"/>
+                        </div>
+                        <div className="col-md-4 mb-4">
+                            <img src="/about/photo5.JPG" alt="Photo 1" className="img-fluid rounded"/>
+                        </div>
+                        <div className="col-md-4 mb-4">
+                            <img src="/about/photo6.JPG" alt="Photo 1" className="img-fluid rounded"/>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <footer className="footer mt-5 py-3 bg-light">
+            <footer className="mt-5 py-3" style={{backgroundColor: '#B47978', color: '#ffffff'}}>
                 <div className="container text-center">
-                    <span className="text-muted">Connect with me on <a href="https://www.linkedin.com/in/tehreem-nazar-b30724183/" target="_blank">LinkedIn</a>.</span>
+                    <span>Connect with me on <a href="https://www.linkedin.com/in/tehreem-nazar-b30724183/" target="_blank" rel="noopener noreferrer" style={{color: '#0d6efd'}}>LinkedIn</a>.</span>
                 </div>
             </footer>
         </>
-    )
+    );
 }
