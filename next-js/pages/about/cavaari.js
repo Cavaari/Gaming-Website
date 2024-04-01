f0ae18539ee5b6ea6b33d8d894c0d9d9242dd5ce
Cavaari <cavaarit@gmail.com>
2024-04-01 10:16:51 -0400

    about me styling

--- next-js/pages/about/cavaari.js
+++ next-js/pages/about/cavaari.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import { useEffect } from "react"
+import Image from 'next/image';
 
 export default function About() {
     useEffect(()=>{
@@ -26,12 +27,14 @@ export default function About() {
                         with beautiful people. I am lived in this beautiful place for many years now i'll share that beauty 
                         with you. Here is my island, Saint Lucia.
                     </p>
-                    <div className="image-container">
-                        <image src="/about/Pitons.jpg" alt="Pitons" className="form-image"/>
-                        <image src="/about/Marigot.jpg" alt="Marigot" className="form-image"/>
-                        <image src="/about/Sunset.jpg" alt="Sunset" className="form-image"/>
-                        <image src="/about/Ciceron.jpg" alt="Ciceron" className="form-image"/>
+                    <div className="col-lg-4 mb-4" style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                      <Image width={400} height={400} src="/about/Pitons.jpg" alt="Pitons" className="img-fluid mb-2" style={{ borderColor: '#ddd', borderWidth: '5px' }}/>
+                      <Image width={400} height={400} src="/about/Marigot.jpg" alt="Marigot" className="img-fluid mb-2" style={{ borderColor: '#ddd', borderWidth: '5px' }}/>
+                      <Image width={400} height={400} src="/about/Sunset.jpg" alt="Sunset" className="img-fluid mb-2" style={{ borderColor: '#ddd', borderWidth: '5px' }}/>
+                      <Image width={400} height={400} src="/about/Ciceron.jpg" alt="Ciceron" className="img-fluid mb-2" style={{ borderColor: '#ddd', borderWidth: '5px' }}/>
                     </div>
+
+
                 </div>
 
             </main>
